86ddf71bc239f14f8e334a6d04a9cedee213e128
mandal-sneha <mandal.sneha174@gmail.com>
2025-07-22 19:04:16 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,11 @@ import DashboardHome from './components/dashboardcomponents/DashboardHome.jsx';
 import WaterRegistration from './components/dashboardcomponents/WaterRegistration.jsx';
 import AddProperty from './components/dashboardcomponents/AddProperty.jsx';
 import UsageInsights from'./components/dashboardcomponents/UsageInsights.jsx';
+import Profile from './components/Profile.jsx';
+import ViewInvitation from'./components/dashboardcomponents/ViewInvitation.jsx';
 import AddPropertyForm from './components/dashboardcomponents/addpropertycomponents/AddPropertyForm.jsx';
 import PropertyTenants from './components/dashboardcomponents/addpropertycomponents/PropertyTenants.jsx';
+
 
 const App = () => {
   return (
@@ -19,6 +22,16 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+         <Route
+      path="/u/:userid/profile"
+      element={
+        <ProtectedRoute>
+          <ThemeProvider>
+            <Profile />
+          </ThemeProvider>
+        </ProtectedRoute>
+      }
+    />
         <Route
           path="/u/:userid"
           element={
@@ -30,11 +43,14 @@ const App = () => {
           }
         >
           <Route index element={<DashboardHome />} />
+          
           <Route path="water-registration" element={<WaterRegistration />} />
           <Route path="add-property" element={<AddProperty />} /> 
           <Route path="usage-insights" element={<UsageInsights/>} />         
           <Route path="add-property-form" element={<AddPropertyForm />} />
           <Route path="property-tenants" element={<PropertyTenants/>} />
+          
+
           
         </Route>
       </Routes>
